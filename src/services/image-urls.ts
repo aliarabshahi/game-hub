527e4528aebf31a40bceba5e45a81c251ebd3da5
ali ara <user@alis-Mac-mini.local>
2025-04-24 15:04:18 +0330

    Handle games with no image

--- src/services/image-urls.ts
+++ src/services/image-urls.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 export const getCroppedImageUrl = (url: string) => {
-    if (!url) return "";
-    const index = url.indexOf("media/") + "media/".length;
-    return url.slice(0, index) + "crop/600/400/" + url.slice(index);
-  };
+  if (!url) return noImage;
+  const index = url.indexOf("media/") + "media/".length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
