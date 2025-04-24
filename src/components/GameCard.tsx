bf8c3cbaeaff5383d7c2714999602e652f0bb14d
ali ara <user@alis-Mac-mini.local>
2025-04-24 06:51:34 +0330

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,7 +5,7 @@ import { Card } from "@chakra-ui/react";
 import { CardBody } from "@chakra-ui/react";
 import { PlatformIconList } from "./PlatformIconList";
 import { CriticScore } from "./CriticScore";
-import  {getCroppedImageUrl } from "../services/image-urls";
+import { getCroppedImageUrl } from "../services/image-urls";
 
 interface Props {
   // Define the props for the GameCard component here
@@ -13,17 +13,15 @@ interface Props {
 }
 export const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="300px" borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
-
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
-        <CriticScore score={game.metacritic} />
-
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
         </HStack>
       </CardBody>
     </Card>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,27 @@
-import { HStack, Switch, Image, Text, useColorMode, SimpleGrid } from "@chakra-ui/react";
+import {
+  HStack,
+  Switch,
+  Image,
+  Text,
+  useColorMode,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import { GameCard } from "./GameCard";
-     
-export const GameGrid = () => {
+import { GameCardSkeleton } from "./GameCardSkeleton";
 
-    const{ games, error } = useGames();
+export const GameGrid = () => {
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
 
-      <SimpleGrid columns={{sm:1 , md:2, lg:3, xl:5}} spacing={10} >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardSkeleton key={skeleton} />
+          ))}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
